a3ad1525bb9baabcd0af7af7d72eda3afb6a0c88
BrunoSS80 <BrunoSSilva751@gmail.com>
2024-06-16 01:51:03 -0300

    Começando a fazer a habilidade do inimigo dar um dash na direção do player

--- MoveShot/Assets/Scripts/DetectionController.cs
+++ MoveShot/Assets/Scripts/DetectionController.cs
@@ -7,10 +7,17 @@ public class DetectionController : MonoBehaviour
    public string tagTargetDetection = "Player";
 
    public List<Collider2D> detectedObj = new List<Collider2D>();
+   private EnemyMoviment enemyMoviment;
 
+   private void Start() {
+      
+      enemyMoviment = GameObject.Find("Enemy").GetComponent<EnemyMoviment>();
+      
+   }
    private void OnTriggerEnter2D(Collider2D collision) {
       if(collision.gameObject.tag == tagTargetDetection){
          detectedObj.Add(collision);
+         enemyMoviment.VoarPlayer();
       }
    }
 
--- MoveShot/Assets/Scripts/Enemy Scripts/EnemyMoviment.cs
+++ MoveShot/Assets/Scripts/Enemy Scripts/EnemyMoviment.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class EnemyMoviment : MonoBehaviour
 {
     public float speedEnemy = 3.5f;
+    public float distanceDash = 10.0f;
     private Vector2 enemydirection;
     private Rigidbody2D enemyRB;
     private SpriteRenderer spriteRenderer;
     public DetectionController detectionController;
+    public Transform player;
     
 
     // Start is called before the first frame update
@@ -25,10 +27,13 @@ public class EnemyMoviment : MonoBehaviour
 
     private void FixedUpdate() {
         
-        enemydirection = (detectionController.detectedObj[0].transform.position - transform.position).normalized;
-
+        //enemydirection = (detectionController.detectedObj[0].transform.position - transform.position).normalized;
+        enemydirection = (player.transform.position - transform.position).normalized;
         enemyRB.MovePosition(enemyRB.position + enemydirection * speedEnemy * Time.fixedDeltaTime);
         
+        if(player.transform.position == transform.position){
+            Debug.Log("parou");
+        }
 
         if(enemydirection.x > 0){
             spriteRenderer.flipX = false;
@@ -36,5 +41,15 @@ public class EnemyMoviment : MonoBehaviour
         if(enemydirection.x < 0){
             spriteRenderer.flipX = true;
         }
+    }
+
+    public void VoarPlayer(){
+        StartCoroutine(Espera());
+        //transform.position += (Vector3)enemydirection * distanceDash;
+    }
+    IEnumerator Espera(){
+        transform.position += transform.position * 0;
+        yield return new WaitForSeconds(5.0f);
+        transform.position = player.transform.position;
     }
 }
