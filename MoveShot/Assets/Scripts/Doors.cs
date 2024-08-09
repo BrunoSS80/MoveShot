8f1b0e807c4a952eb01d87fd38f1579190a51a4a
BrunoSS80 <BrunoSSilva751@gmail.com>
2024-08-09 16:23:23 -0300

    Fazendo animação da camera se apagar ao trocar de sala.

--- MoveShot/Assets/Scripts/Doors.cs
+++ MoveShot/Assets/Scripts/Doors.cs
@@ -7,13 +7,27 @@ public class Doors : MonoBehaviour
 {
     private Transform player;
     public Vector3 walkTo;
+    private float durationWalk = 5;
+    public Animator animatorPanel;
 
     private void Start() {
         player = GameObject.Find("Player").GetComponent<Transform>();
+        animatorPanel = GameObject.Find("Panel").GetComponent<Animator>();
     }
+
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")){
-            player.transform.position = Vector2.Lerp (player.transform.position, walkTo, Time.deltaTime);
+            StartCoroutine(ChangeTimer());
         }
+    }
+
+    IEnumerator ChangeTimer(){
+        animatorPanel.SetBool("FadeIn", true);
+        yield return new WaitForSeconds(1);
+        player.transform.position = Vector2.Lerp(player.transform.position, walkTo, durationWalk);
+        animatorPanel.SetBool("FadeOut", true);
+        yield return new WaitForSeconds(1);
+        animatorPanel.SetBool("FadeIn", false);
+        animatorPanel.SetBool("FadeOut", false);
     }
 }
