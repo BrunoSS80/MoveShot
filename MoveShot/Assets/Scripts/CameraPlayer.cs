c43439126ce598fbb9b00a1ca440c7ab8761c80f
BrunoSS80 <BrunoSSilva751@gmail.com>
2024-08-17 18:10:45 -0300

    Finalizado sistema para a camera seguir o player para a sala que ele for.

--- MoveShot/Assets/Scripts/CameraPlayer.cs
+++ MoveShot/Assets/Scripts/CameraPlayer.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class CameraPlayer : MonoBehaviour
 {
     public float dampTime = 0.15f;
-    private Vector3 velocity = Vector3.zero;
-    public Transform objetoParaSeguir;
+    public GameObject objFollow;
     
     void Update () {
-        Vector3 point = GetComponent<Camera>().WorldToViewportPoint(objetoParaSeguir.position);
-        Vector3 delta = objetoParaSeguir.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3 (0.5f, 0.5f, point.z));
-        Vector3 destination = transform.position + delta;
-        transform.position = Vector3.SmoothDamp (transform.position, destination, ref velocity, dampTime);
+        
+    }
+
+    public void MoveCamera(){
+        transform.position = Vector3.Lerp(transform.position, objFollow.transform.position, 1);
     }
 }
---
+++ MoveShot/Assets/Scripts/DetectPlayer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour
+{
+    private CameraPlayer cameraPlayer;
+    public GameObject Center;
+
+    private void Start() {
+        cameraPlayer = GameObject.Find("Main Camera").GetComponent<CameraPlayer>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            cameraPlayer.objFollow = Center;
+            cameraPlayer.MoveCamera();
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        
+    }
+}
