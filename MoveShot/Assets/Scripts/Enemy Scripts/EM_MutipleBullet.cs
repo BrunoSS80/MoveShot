0d2d49b3c5e05a47fa92c0e9ece64243cd2db7f6
BrunoSS80 <BrunoSSilva751@gmail.com>
2024-10-10 19:08:59 -0300

    Trilha de tiros feita, todos os tiros seguindo uma linha reta com um intervalo entre eles.

--- MoveShot/Assets/Scripts/Enemy Scripts/EM_MutipleBullet.cs
+++ MoveShot/Assets/Scripts/Enemy Scripts/EM_MutipleBullet.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -18,7 +19,7 @@ public class EM_MutipleBullet : MonoBehaviour
     public Transform[] waypoints;
     private int currentStartPoint;
     private float startTime;
-    public float speed;
+    public float speed, spawnBullet;
     private float journeyLenght;
     private void Start() {
         weaponAnimator = GetComponent<Animator>();
@@ -29,36 +30,52 @@ public class EM_MutipleBullet : MonoBehaviour
     private void Update() {
         if(PodeAtirar()){
             BulletInstatiate();
-            BulletTrail();
         }
-        float distCovered = (Time.time - startTime) * speed;
-        float fracJourney = distCovered / journeyLenght;
-        aimBoss.transform.position = Vector2.Lerp(startPosAim.position, endPosAim.position, fracJourney);
-        if(fracJourney >= 1 && currentStartPoint + 1 < waypoints.Length){
-            currentStartPoint++;
-            SetPoints();
-        }
-    }
-    public void BulletInstatiate(){
-    float speedProject = 1.0f;
-    int width = patternTexture.width;
-    int height = patternTexture.height;
-    Vector2 centerMatriz = new Vector2(width/2, height/2);
-    fireTimer = Time.time + fireRate;
+            //for(int i = 0; i < 4; i++){
+                spawnBullet -= Time.deltaTime * 6;
+                float distCovered = (Time.time - startTime) * speed;
+                float fracJourney = distCovered / journeyLenght;
 
-    for(int y = 0; y < height; y++){
-        for(int x = 0; x < width; x++){
-            Color pixelColor = patternTexture.GetPixel(x,y);
-            
-            if(pixelColor == Color.black){
-                Vector2 position = barrel.position + (new Vector3(x - width/2, y - height/2));
-                direction = new Vector2(x,y) - centerMatriz;
-                Rigidbody2D projectile = Instantiate(projectilePrefab, position, barrel.rotation).GetComponent<Rigidbody2D>();
-                projectile.velocity = direction * speedProject;
-                weaponAnimator.SetTrigger("Fire");
+                aimBoss.transform.position = Vector2.Lerp(startPosAim.position, endPosAim.position, fracJourney);
+                
+
+                    if(fracJourney >= 1 && currentStartPoint + 1 < waypoints.Length){
+                        currentStartPoint++;
+                        SetPoints();
+                    }
+                    if(spawnBullet <= 0){
+                        spawnBullet = 1;
+                        Instantiate(projectMoving, aimBoss.transform.position, barrel.rotation);
+                    }
+                    if(currentStartPoint == 4){
+                        currentStartPoint = 0;
+                    }
+            //}
+            }
+        
+    
+    public void BulletInstatiate(){
+    //for(int i = 0; i < 5; i++){
+        float speedProject = 1.0f;
+        int width = patternTexture.width;
+        int height = patternTexture.height;
+        Vector2 centerMatriz = new Vector2(width/2, height/2);
+        fireTimer = Time.time + fireRate;
+
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                Color pixelColor = patternTexture.GetPixel(x,y);
+                
+                if(pixelColor == Color.black){
+                    Vector2 position = barrel.position + (new Vector3(x - width/2, y - height/2));
+                    direction = new Vector2(x,y) - centerMatriz;
+                    Rigidbody2D projectile = Instantiate(projectilePrefab, position, barrel.rotation).GetComponent<Rigidbody2D>();
+                    projectile.velocity = direction * speedProject;
+                    weaponAnimator.SetTrigger("Fire");
+                }
             }
         }
-    }
+    //}
     }
 
     private bool PodeAtirar(){
@@ -72,8 +89,26 @@ public class EM_MutipleBullet : MonoBehaviour
     }
 
     private void BulletTrail(){
-        while(true){
-            Instantiate(projectMoving, aimBoss.transform.position, barrel.rotation);
-        }
+    for(int i = 0; i < 4; i++){
+        spawnBullet -= Time.deltaTime * 6;
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLenght;
+
+        aimBoss.transform.position = Vector2.Lerp(startPosAim.position, endPosAim.position, fracJourney);
+        fireTimer = Time.time + fireRate;
+
+            if(fracJourney >= 1 && currentStartPoint + 1 < waypoints.Length){
+                currentStartPoint++;
+                SetPoints();
+            }
+            if(spawnBullet <= 0){
+                spawnBullet = 1;
+                Instantiate(projectMoving, aimBoss.transform.position, barrel.rotation);
+            }
+            if(currentStartPoint == 4){
+                currentStartPoint = 0;
+            }
+    }
+    fireTimer = Time.time + fireRate;
     }
 }
